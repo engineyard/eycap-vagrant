94478ca34421d7bcf8733c2a33fb3f0a472df173
Tyler Bird <tyler.bird@gmail.com>
2014-08-24 01:59:06 -0600

    add Berkshelf support

---
+++ Berksfile
@@ -0,0 +1,3 @@
+source "https://api.berkshelf.com"
+
+cookbook 'build-essential', github: 'opscode-cookbooks/build-essential'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'eycap-vagrant'
+maintainer       'Tyler Bird'
+maintainer_email 'tbird@engineyard.com'
+license          'All rights reserved'
+description      'eycap-vagrant locally tests eycap'
+long_description 'Use this tool to test changes to the eycap gem, while you develop new features or recipes.'
+version          '0.1.0'
+
